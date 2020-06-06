afc999ef3f47c0f9052d27c4f522f68c3638379b
Krystal Bailey <krystalmbai33@gmail.com>
2020-06-06 04:24:36 -0400

    Made Index Page and Started Homepage

---
+++ js/bwls.js
@@ -0,0 +1,14 @@
+ /* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+  
+  $('.carousel').carousel({
+    interval: 3000
+  })
+
+  $('#myCarousel').on('slide.bs.carousel', function () {
+    // do something
+  })
+
+ 
